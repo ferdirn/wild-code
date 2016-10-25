c94365313ede0d094c063a411a3c116b3f0d56b7
Ferdi Ramdhon <ferdirn@gmail.com>
2016-10-25 10:47:08 +0700

    ionic lists

---
+++ ionic-lists/app.js
@@ -0,0 +1,17 @@
+angular.module('app', ['ionic'])
+
+.controller('AppCtrl', function($scope) {
+  $scope.items = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 }
+  ];
+});
+
