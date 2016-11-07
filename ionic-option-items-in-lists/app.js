44491a7b571125094a843a8a134467d7c9529872
Ferdi Ramdhon <ferdirn@gmail.com>
2016-11-07 13:32:14 +0700

    ionic option items in lists

---
+++ ionic-option-items-in-lists/app.js
@@ -0,0 +1,26 @@
+angular.module('app', ['ionic'])
+
+.controller('AppCtrl', function($scope) {
+
+  $scope.items = [
+    { id: 0 },
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 }
+  ];
+  
+  $scope.edit = function(item) {
+    alert('Edit Item: ' + item.id);
+  };
+  $scope.share = function(item) {
+    alert('Share Item: ' + item.id);
+  };
+});
+
